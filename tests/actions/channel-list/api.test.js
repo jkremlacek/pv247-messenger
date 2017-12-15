0cafbc20796dbbdb56b5c981c971ba155d15fc0f
jkremlacek <jkremlacek@email.cz>
2017-12-15 23:05:01 +0100

    Tests: Channel api tests

--- tests/actions/channel-list/api.test.js
+++ tests/actions/channel-list/api.test.js
@@ -4,18 +4,18 @@ import {
     startProcessingChannelList,
     updateLocalChannelList
 } from '../../../src/actions/channel-list/actionCreators';
-import {fetchRemoteChannelList} from '../../../src/actions/channel-list/api';
+import {
+    addRemoteChannel,
+    fetchRemoteChannelList,
+    removeRemoteChannel,
+    updateRemoteChannel
+} from '../../../src/actions/channel-list/api';
 import {convertFromServerDetails as getChannelList} from '../../../src/utils/api/conversions/channelList';
+import Immutable from "immutable";
 
 test('fetch channel list test', async (done) => {
     const dispatch = jest.fn(action => action);
-    const getState = () => ({
-        shared: {
-            token: {
-                value: 'defaultToken'
-            },
-        }
-    });
+    const getState = () => ({shared: {token: {value: 'defaultToken'},}});
 
     const serverReturnValue = {
         "id":"cd89d662-0045-4521-8ff5-6db84451d9fb",
@@ -37,5 +37,89 @@ test('fetch channel list test', async (done) => {
     expect(dispatch).toBeCalledWith(startProcessingChannelList());
     expect(dispatch).toBeCalledWith(updateLocalChannelList(expectedValue));
     expect(dispatch).toHaveBeenLastCalledWith(endProcessingChannelList());
+    done();
+});
+
+test('update channel test', async(done) => {
+    const dispatch = jest.fn(action => action);
+    const getState = () => ({shared: {token: {value: 'defaultToken'},}});
+
+    const channel = {
+        id: "001",
+        title: "TestChannel",
+        ownerId: "test@user.com",
+        members: Immutable.Set(["test@user.com", "test02@user.com"]),
+    };
+
+    var requestBody = "X";
+
+    const dispatchable = updateRemoteChannel(channel, (a,b,c) => {requestBody = c;});
+    await dispatchable(dispatch, getState);
+
+    expect(dispatch).toBeCalledWith(startProcessingChannelList());
+    expect(dispatch).toBeCalledWith(endProcessingChannelList());
+
+    expect([{
+                "op": "replace",
+                "path": "/channels/001",
+                "value": {"customData": "{\"id\":\"001\",\"title\":\"TestChannel\",\"ownerId\":\"test@user.com\",\"members\":[\"test@user.com\",\"test02@user.com\"]}", "id": "001", "name": "TestChannel"}
+            }]).toEqual(requestBody);
+
+    done();
+});
+
+test('delete channel test', async(done) => {
+    const dispatch = jest.fn(action => action);
+    const getState = () => ({shared: {token: {value: 'defaultToken'},}});
+
+    const channel = {
+        id: "001",
+        title: "TestChannel",
+        ownerId: "test@user.com",
+        members: Immutable.Set(["test@user.com", "test02@user.com"]),
+    };
+
+    var requestBody = "X";
+
+    const dispatchable = removeRemoteChannel(channel, (a,b,c) => {requestBody = c;});
+    await dispatchable(dispatch, getState);
+
+    expect(dispatch).toBeCalledWith(startProcessingChannelList());
+    expect(dispatch).toBeCalledWith(endProcessingChannelList());
+
+    expect([{
+        "op": "remove",
+        "path": "/channels/001",
+        "value": {"customData": "{\"id\":\"001\",\"title\":\"TestChannel\",\"ownerId\":\"test@user.com\",\"members\":[\"test@user.com\",\"test02@user.com\"]}", "id": "001", "name": "TestChannel"}
+    }]).toEqual(requestBody);
+
+    done();
+});
+
+test('add channel test', async(done) => {
+    const dispatch = jest.fn(action => action);
+    const getState = () => ({shared: {token: {value: 'defaultToken'},}});
+
+    const channel = {
+        id: "001",
+        title: "TestChannel",
+        ownerId: "test@user.com",
+        members: Immutable.Set(["test@user.com", "test02@user.com"]),
+    };
+
+    var requestBody = "X";
+
+    const dispatchable = addRemoteChannel(channel, (a,b,c) => {requestBody = c;});
+    await dispatchable(dispatch, getState);
+
+    expect(dispatch).toBeCalledWith(startProcessingChannelList());
+    expect(dispatch).toBeCalledWith(endProcessingChannelList());
+
+    expect([{
+        "op": "add",
+        "path": "/channels/-",
+        "value": {"customData": "{\"id\":\"001\",\"title\":\"TestChannel\",\"ownerId\":\"test@user.com\",\"members\":[\"test@user.com\",\"test02@user.com\"]}", "id": "001", "name": "TestChannel"}
+    }]).toEqual(requestBody);
+
     done();
 });
